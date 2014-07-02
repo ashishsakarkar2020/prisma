095cff0ed2bc704205aa4a1b241877ed1c933aa1
wab [odev] <wab2com@gmail.com>
2014-07-02 17:40:10 +0200

    install templates

--- wp-content/themes/theme-prisma/page-section.php
+++ wp-content/themes/theme-prisma/page-section.php
@@ -2,29 +2,29 @@
 /*
 Template Name: Section Page
 */
+
+$parentid = $post->ID;
+// on récupère l'id de la page courante soit la page parente
+$args = array(
+    'post_type' => 'page',
+    'numberposts' => -1, // -1 signifie toutes les sous-pages
+    'post_parent' => $parentid // numéro de la page parente
+    );
+$posts = get_posts($args);
+
 ?>
+<?php get_template_part('templates/page', 'header'); ?>
 
+<?php the_content() ; ?>
 
-<section class="section">
-
-	<?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page'); ?>
-	<ul class="nav nav-section">
-	<li><a href="#" class="prev-section">prev</a></li>
-	<li><a href="#" class="next-section">next</a></li>
-</ul>
-</section>
-<section class="section">
-	<h2>Section 2</h2>
-	<ul class="nav nav-section">
-	<li><a href="#" class="prev-section">prev</a></li>
-	<li><a href="#" class="next-section">next</a></li>
-</ul>
-</section>
-<section class="section">
-	<h2>Section 3</h2>
-	<ul class="nav nav-section">
-	<li><a href="#" class="prev-section">prev</a></li>
-	<li><a href="#" class="next-section">next</a></li>
-</ul>
-</section>
+<?php foreach ($posts as $post) :
+    setup_postdata($post); ?>
+    <section class="section">
+	    <h2><?php  the_title(); ?></h2>
+	    <?php the_content() ; ?>
+	    <ul class="nav nav-section">
+			<li><a href="#" class="prev-section">prev</a></li>
+			<li><a href="#" class="next-section">next</a></li>
+		</ul>
+    </section>
+<?php endforeach; ?>
---
+++ wp-content/themes/theme-prisma/page-slider.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Slider
+*/
+?>
+
+
+
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page'); ?>
+	
