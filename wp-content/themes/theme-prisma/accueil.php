f2e2b5c0a04717e3f307a12ab4b400b86041da4b
wab <contact@wabdesign.fr>
2014-07-14 23:34:55 +0200

    int page accueil

--- wp-content/themes/theme-prisma/accueil.php
+++ wp-content/themes/theme-prisma/accueil.php
@@ -9,11 +9,16 @@ $actus = array(
     'numberposts' => 2
     );
 
+// especes
+$especes = array(
+    'post_type' => 'especes'
+    );
+
 $services = array(
   'menu'            => 'services',
   'container'       => false,
-  'menu_class'      => 'level-3 nav',
-  'items_wrap'      => '<ol id="%1$s" class="%2$s">%3$s</ol>',
+  'menu_class'      => 'nav',
+  'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
   'depth'           => 0,
   'walker'          => ''
 );
@@ -21,8 +26,8 @@ $services = array(
 $produits = array(
   'menu'            => 'produits',
   'container'       => false,
-  'menu_class'      => 'level-3 nav',
-  'items_wrap'      => '<ol id="%1$s" class="%2$s">%3$s</ol>',
+  'menu_class'      => 'nav',
+  'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
   'depth'           => 0,
   'walker'          => ''
 );
@@ -31,105 +36,322 @@ wp_reset_postdata();
 
 ?>
 
+<!-- Slider -->
+
 <section class="section">
+
+
+<?php
+
+// check if the flexible content field has rows of data
+if( have_rows('slider') ):
+ 
     
-    <?php
+?>
 
-    $images = get_field('slider');
+    <div id="slider" class="slider">
+
+    <?php     // loop through the rows of data
+    while ( have_rows('slider') ) : the_row(); ?>
+        
      
-    if( $images ): ?>
-
-        <div id="slider" class="slider">
-
-            <?php foreach( $images as $image ): ?>
-                <div>
-                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-responsive" />
+        <?php if( get_row_layout() == 'slide' ):?>
+            <div class="slide" style="background-image: url('<?php the_sub_field('image'); ?>')">
+                <div class="row">
+                    <div class="col-xs-4 col-xs-offset-7">
+                        <h1><?php the_sub_field('titre'); ?></h1>
+                        <hr>
+                        <h2><?php the_sub_field('sous_titre'); ?></h2>
+                        <p><a href="<?php the_sub_field('lien'); ?>"class="btn btn-primary">Lire la suite</a></p>
+                    </div>
                 </div>
-            <?php endforeach; ?>
-        </div>
+            </div>        
+        <?php endif; ?>
+    
 
-    <?php endif; ?>
+    <?php endwhile; ?>
 
-    <?php the_content() ; ?>
-
-    <!-- <div class="nav-count">5/5</div> -->
+    </div>
+        
+<?php endif; ?>        
 
 </section>
 
 <section class="section">
     <div class="row">
+
+        <!-- Actus-->
+
         <div class="col-xs-6">
-            <?php $query_actus = new WP_Query( $actus ); ?>
+            <div class="actus">
+                <?php 
+                    $query_actus = new WP_Query( $actus );
+                    $a = 0;
+                ?>
 
-            <!-- the loop -->
-            <?php while ( $query_actus->have_posts() ) : $query_actus->the_post(); ?>
-            <h2><?php the_title(); ?></h2>            
-            <?php // if ( has_post_thumbnail() ) { ?>
-               <?php the_post_thumbnail('thumbnail', array('class' => 'img-responsive')); ?>
-              <?php //   } ?>
-            <?php the_excerpt() ; ?>
-            <a href="<?php the_permalink() ; ?>" class="btn btn-primary">lire la suite</a>
-            <?php endwhile; ?>
-            <!-- end of the loop -->
+                <h2 class="h1 text-info text-center"><span class="icon icon-pin"></span>Actualités</h2>
+                <h3 class="text-center">nouveautés &amp; évènements</h3>
 
-            <!-- pagination here -->
+                <!-- the loop -->
+                <?php 
+                    while ( $query_actus->have_posts() ) : 
+                    $query_actus->the_post(); 
+                    $a = $a + 1;
+                    ?>
+                
+                <article <?php post_class(); ?>>
 
-            <?php wp_reset_postdata(); ?>
+                    <header class="page-header">
+                        <p class="h3 meta">14/07/14 | <small>theme</small></p>
+                        <h2 class="h3"><?php the_title(); ?></h2>
+                    </header>  
+
+                    <?php  if ($a == 1) { ?>
+                       <?php the_post_thumbnail('actus', array('class' => 'img-responsive')); ?>
+                      <?php   } ?>
+                    <p><?php the_excerpt() ; ?></p>
+                    <a href="<?php the_permalink() ; ?>" class="btn btn-primary">lire la suite</a>
+                </article>
+                <?php endwhile; ?>
+                <!-- end of the loop -->
+
+                <?php wp_reset_postdata(); ?>
+            </div>
         </div>
+        
+        <!-- Section Accès directs -->
+
         <div class="col-xs-6">
+
             <div class="row">
                 <div class="col-xs-6">
-                    <div class="acces wrapper">
-                        <h2>Titre</h2>
-                        <p>Aliquam nisl enim, tristique tempus placerat at, posuere in lectus. Suspendisse potenti cras molestie, risus a enim convallis vitae luctus libero lacinia. Nulla auctor eleifend turpis consequat pharetra. Donec et nisi dictum felis sollicitudin congue. Donec a congue leo. </p>
+                    <div class="acces">
+                        
+                        <!-- prisma-->
+
+                        <?php $query_prisma = new WP_Query( 'page_id=9' ); ?>
+
+                        <!-- the loop -->
+                        <?php while ( $query_prisma->have_posts() ) : $query_prisma->the_post(); ?>
+                        <a href="<?php the_permalink() ; ?>" class="btn-info">
+                            <p class="text-center h1"><span class="icon icon-prisma"></span></p>
+                            <h2 class="h3 text-center"><?php the_title(); ?></h2>            
+                            <?php the_excerpt() ; ?>
+                        </a>
+                        <?php endwhile; ?>
+                        <!-- end of the loop -->
+                        <?php wp_reset_postdata(); ?>
                     </div>
                 </div>
                 <div class="col-xs-6">
-                    <div class="acces wrapper">
-                        <h2>Titre</h2>
-                        <p>Aliquam nisl enim, tristique tempus placerat at, posuere in lectus. Suspendisse potenti cras molestie, risus a enim convallis vitae luctus libero lacinia. Nulla auctor eleifend turpis consequat pharetra. Donec et nisi dictum felis sollicitudin congue. Donec a congue leo. </p>
+                    <div class="acces">
+                        
+                        <!-- l'équipe-->
+
+                        <?php $query_prisma = new WP_Query( 'page_id=11' ); ?>
+
+                        <!-- the loop -->
+                        <?php while ( $query_prisma->have_posts() ) : $query_prisma->the_post(); ?>
+                        <a href="<?php the_permalink() ; ?>" class="btn-warning">
+                            <p class="text-center h1"><span class="icon icon-users"></span></p>
+                            <h2 class="h3 text-center"><?php the_title(); ?></h2>            
+                            <?php the_excerpt() ; ?>
+                        </a>
+                        <?php endwhile; ?>
+                        <!-- end of the loop -->
+                        <?php wp_reset_postdata(); ?>
                     </div>
                 </div>
                 <div class="col-xs-6">
-                    <div class="acces wrapper">
-                        <h2>Titre</h2>
-                        <p>Aliquam nisl enim, tristique tempus placerat at, posuere in lectus. Suspendisse potenti cras molestie, risus a enim convallis vitae luctus libero lacinia. Nulla auctor eleifend turpis consequat pharetra. Donec et nisi dictum felis sollicitudin congue. Donec a congue leo. </p>
+                    <div class="acces">
+                        
+                        <!-- Comment travaillons-nous ? -->
+
+                        <?php $query_prisma = new WP_Query( 'page_id=13' ); ?>
+
+                        <!-- the loop -->
+                        <?php while ( $query_prisma->have_posts() ) : $query_prisma->the_post(); ?>
+                        <a href="<?php the_permalink() ; ?>" class="btn-primary">
+                            <p class="text-center h1"><span class="icon icon-factory"></span></p>
+                            <h2 class="h3 text-center"><?php the_title(); ?></h2>            
+                            <?php the_excerpt() ; ?>
+
+                        </a>
+                        <?php endwhile; ?>
+                        <!-- end of the loop -->
+                        <?php wp_reset_postdata(); ?>
                     </div>
                 </div>
                 <div class="col-xs-6">
-                    <div class="acces wrapper">
-                        <h2>Titre</h2>
-                        <p>Aliquam nisl enim, tristique tempus placerat at, posuere in lectus. Suspendisse potenti cras molestie, risus a enim convallis vitae luctus libero lacinia. Nulla auctor eleifend turpis consequat pharetra. Donec et nisi dictum felis sollicitudin congue. Donec a congue leo. </p>
+                    <div class="acces">
+                        
+                        <!-- R&D -->
+
+                        <?php $query_prisma = new WP_Query( 'page_id=34' ); ?>
+
+                        <!-- the loop -->
+                        <?php while ( $query_prisma->have_posts() ) : $query_prisma->the_post(); ?>
+                        <a href="<?php the_permalink() ; ?>" class="btn-success">
+                            <p class="text-center h1"><span class="icon icon-retd"></span></p>
+                            <h2 class="h3 text-center"><?php the_title(); ?></h2>            
+                            <?php the_excerpt() ; ?>
+                        </a>
+                        <?php endwhile; ?>
+                        <!-- end of the loop -->
+                        <?php wp_reset_postdata(); ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- <div class="nav-count">5/5</div> -->
 </section>
 
-<section class="section">
-    <div class="row">
-        <div class="col-xs-4 col-xs-offset-8">
-            <?php if ( has_nav_menu( 'services' ) ) {wp_nav_menu($services); }; ?>
-        </div>
-    </div>
-    <!-- <div class="nav-count">5/5</div> -->
-</section>
+<!-- Section Services -->
 
-<section class="section">
-    <div class="row">
-        <div class="col-xs-4 col-xs-offset-8">
+<?php
+
+// check if the flexible content field has rows of data
+if( have_rows('services') ):
+ 
+    
+?>
+
+    <?php     // loop through the rows of data
+    while ( have_rows('services') ) : the_row(); ?>
+        
+        
+     
+        <?php if( get_row_layout() == 'intro' ):?>
+        <section class="section services" style="background-image: url('<?php the_sub_field('image'); ?>')">
+            
+                <div class="row">
+                    <div class="col-xs-6 col-xs-offset-1 text-right">
+                        <h1 class="text-primary"><span class="icon icon-gear"></span> services</h1>
+                        <h2><?php the_sub_field('sous_titre'); ?></h2>     
+                        <p><?php the_sub_field('description'); ?></p>
+                    </div>
+
+                    <div class="col-xs-offset-1 col-xs-4">
+                    <?php if ( has_nav_menu( 'services' ) ) {wp_nav_menu($services); }; ?>
+                    </div>
+                </div>
+
+    </section>
+     
+        <?php endif; ?>
+    
+
+    <?php endwhile; ?>
+        
+<?php endif; ?>
+
+<!-- Section produits -->
+
+<?php
+
+// check if the flexible content field has rows of data
+if( have_rows('produits') ):
+ 
+    
+?>
+
+    <?php     // loop through the rows of data
+    while ( have_rows('produits') ) : the_row(); ?>
+        
+        
+     
+        <?php if( get_row_layout() == 'intro' ):?>
+        <section class="section produits" class="section services" style="background-image: url('<?php the_sub_field('image'); ?>')">
+            
+            <div class="row">
+                <div class="col-xs-6 col-xs-offset-1 text-right">
+                <h1><span class="icon icon-produits"></span> produits</h1>
+                <h2><?php the_sub_field('sous_titre'); ?></h2>     
+                <p><?php the_sub_field('description'); ?></p>
+            </div>
+
+            <div class="col-xs-offset-1 col-xs-4">
             <?php if ( has_nav_menu( 'produits' ) ) {wp_nav_menu($produits); }; ?>
+            </div>
         </div>
-    </div>
-    <!-- <div class="nav-count">5/5</div> -->
-</section>
+    </section>
+     
+        <?php endif; ?>
+    
 
-<section class="section">
-    <h2><?php  the_title(); ?></h2>
-    <?php the_content() ; ?>
-</section>
+    <?php endwhile; ?>
+        
+<?php endif; ?>
+
+<!-- Section espèces -->
+
+<?php
+
+// check if the flexible content field has rows of data
+if( have_rows('especes') ):
+ 
+    
+?>
+
+    <?php     // loop through the rows of data
+    while ( have_rows('especes') ) : the_row(); ?>
+        
+        
+     
+        <?php if( get_row_layout() == 'intro' ):?>
+        <section class="section especes" class="section services">
+            
+            <div class="row">
+
+                <div class="col-xs-4 col-xs-offset-1 text-right">
+                    <h1><span class="icon icon-especes"></span> Espèces</h1>
+                    <h2><?php the_sub_field('sous_titre'); ?></h2>
+                </div>
+
+                <div class="col-xs-6"> 
+                    <p class="desc"><?php the_sub_field('description'); ?></p>
+                </div>
+
+            </div>
+
+            <div id="slider-especes" class="carousel">
+
+                <?php $query_especes = new WP_Query( $especes ); ?>
+
+                <!-- the loop -->
+                <?php while ( $query_especes->have_posts() ) : $query_especes->the_post(); ?>
+
+                <div class="slide text-center">
+
+                            
+                    
+                    <?php // if ( has_post_thumbnail() ) { ?>
+                    <a href="<?php the_permalink() ; ?>">
+                       <?php the_post_thumbnail('square', array('class' => 'img-responsive')); ?>
+                    </a>
+                    <?php //   } ?>
+
+                    <h2><?php the_title(); ?></h2>  
+
+                </div>
+                
+                
+                <?php endwhile; ?>
+                <!-- end of the loop -->
+
+            <?php wp_reset_postdata(); ?>
+
+            </div>
+
+            
+    </section>
+     
+        <?php endif; ?>
+    
+
+    <?php endwhile; ?>
+        
+<?php endif; ?>
 
 <div class="nav-section arrow" data-spy="affix" data-offset-top="120">
     <a href="#" class="prev-section"><span class="icon icon-arrow-up"></span></a>
