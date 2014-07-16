7b112dbf89c50e49cb4dae0f0906daa7d70157a0
wab [odev] <wab2com@gmail.com>
2014-07-16 18:28:58 +0200

    if ie

--- wp-content/themes/theme-prisma/templates/head.php
+++ wp-content/themes/theme-prisma/templates/head.php
@@ -4,8 +4,10 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
-<!--   <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+  <!--   <meta name="viewport" content="width=device-width, initial-scale=1"> -->
   <?php wp_head(); ?>
+
+  <!--[if lt IE 9]> <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/ie.css"> <![endif]-->
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
