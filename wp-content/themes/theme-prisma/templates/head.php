472fbb1f30d777a5de9aed814e8df979c207ec18
wab <contact@wabdesign.fr>
2014-08-04 23:38:28 +0200

    favicons

--- wp-content/themes/theme-prisma/templates/head.php
+++ wp-content/themes/theme-prisma/templates/head.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <!--   <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+  <?php get_template_part('templates/favicons'); ?>
   <?php wp_head(); ?>
 
   <!--[if lt IE 9]> <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/ie.css"> <![endif]-->
