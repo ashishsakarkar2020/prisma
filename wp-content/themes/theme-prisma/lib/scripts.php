3f228d5ce7b634e8ced682b3db960c42d02b1b7c
wab [odev] <wab2com@gmail.com>
2014-08-06 11:24:27 +0200

    enqueue jquery in header

--- wp-content/themes/theme-prisma/lib/scripts.php
+++ wp-content/themes/theme-prisma/lib/scripts.php
@@ -3,7 +3,7 @@
  * Enqueue scripts and stylesheets
  *
  * Enqueue stylesheets in the following order:
- * 1. /theme/assets/css/main.1b57.min.css
+ * 1. /theme/assets/css/main.1abe.min.css
  *
  * Enqueue scripts in the following order:
  * 1. jquery-1.11.0.min.js via Google CDN
@@ -13,7 +13,7 @@
 function roots_scripts() {
 
   
-  wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.1b57.min.css', false, true, 'all');
+  wp_enqueue_style('main', get_template_directory_uri() . '/assets/css/main.1abe.min.css', false, true, 'all');
   wp_enqueue_style('prisma', get_template_directory_uri() . '/assets/css/prisma.css', false, true, 'all');
   wp_enqueue_style('theme', get_template_directory_uri() . '/assets/css/theme.css', false, true, 'all');
   wp_enqueue_style('plugins', get_template_directory_uri() . '/assets/css/plugins.3fc1.min.css', false, true, 'all');
@@ -23,7 +23,7 @@ function roots_scripts() {
   // It's kept in the header instead of footer to avoid conflicts with plugins.
   if (!is_admin() && current_theme_supports('jquery-cdn')) {
     wp_deregister_script('jquery');
-    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), null, true);
+    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js', array(), null, false);
     add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
   }
 
