d3767ddeea4d31fbb4287957d653ad9541ec6673
wab <contact@wabdesign.fr>
2014-09-08 23:00:17 +0200

    marqueur xiti

--- wp-content/themes/theme-prisma/base.php
+++ wp-content/themes/theme-prisma/base.php
@@ -51,5 +51,41 @@
   
   <?php wp_footer(); ?>
 
+  <!-- marqueur Xiti -->
+
+  <?php
+
+  if (is_front_page()) {$xtn2 = 1;}
+  elseif (is_page('9') || is_page('11') || is_page('13') || is_page('15') || is_page('17') ) {$xtn2 = 2;}
+  elseif (is_singular('services') || is_singular('produits')){$xtn2 = 3;}
+  elseif (is_page('34')) {$xtn2 = 4;}
+  elseif (is_page('36')) {$xtn2 = 5;}
+  elseif (is_singular('especes')){$xtn2 = 6;}
+  elseif (is_page('40')) {$xtn2 = 7;}
+  else {$xtn2 = 0;}
+
+  ?>
+
+  <script type="text/javascript">
+    <!--
+    xtnv = document;        //parent.document or top.document or document         
+    xtsd = "http://logc406";
+    xtsite = "550787";
+    xtn2 = "<?php echo $xtn2; ?>";        // level 2 site ID
+    xtpage = "<?php the_title(); ?>";        //page name (with the use of :: to create chapters)
+    xtdi = "";        //implication degree
+    xt_multc = "";    //all the xi indicators (like "&x1=...&x2=....&x3=...")
+    xt_an = "";        //user ID
+    xt_ac = "";        //category ID
+    //do not modify below
+    if (window.xtparam!=null){window.xtparam+="&ac="+xt_ac+"&an="+xt_an+xt_multc;}
+    else{window.xtparam="&ac="+xt_ac+"&an="+xt_an+xt_multc;};
+    //-->
+  </script>
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/xtcore.js"></script>
+  <noscript>
+    <img width="1" height="1" alt="" src="http://logc406.xiti.com/hit.xiti?s=550787&s2=&p=&di=&an=&ac=" >
+  </noscript>
+
 </body>
 </html>
